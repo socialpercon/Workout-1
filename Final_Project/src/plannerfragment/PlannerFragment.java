6b295995f5b3b316d8c8f245e6fd6df5fa98666a
saeryunl <saeryunl@vt.edu>
2013-11-12 23:58:30 -0500

    Planner Fragment Spinner Added

--- Final_Project/src/plannerfragment/PlannerFragment.java
+++ Final_Project/src/plannerfragment/PlannerFragment.java
@@ -1,5 +1,8 @@
 package plannerfragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.final_project.R;
 
 import android.support.v4.app.Fragment;
@@ -7,8 +10,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class PlannerFragment extends Fragment {
+	private Spinner muscleGrpSpinner_;
 
 	public PlannerFragment() {
 
@@ -19,6 +25,18 @@ public class PlannerFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_planner, container,
 				false);
+		muscleGrpSpinner_ = (Spinner) rootView
+				.findViewById(R.id.planner_muscleGroupSpinner);
+		List<String> list = new ArrayList<String>();
+		// Add the muscle group here
+		list.add("list1");
+		list.add("list2");
+		list.add("list3");
+		ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(
+				getActivity(), android.R.layout.simple_spinner_item, list);
+		spinnerAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		muscleGrpSpinner_.setAdapter(spinnerAdapter);
 
 		return rootView;
 	}
