ef7e09a9b27b48837dd044af3ea960380a4590cf
saeryunl <saeryunl@vt.edu>
2013-11-13 01:09:57 -0500

    Planner Fragment ListViews Added

--- Final_Project/gen/com/example/final_project/R.java
+++ Final_Project/gen/com/example/final_project/R.java
@@ -24,35 +24,37 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f09001c;
-        public static final int btn_Show=0x7f090014;
-        public static final int btn_cancel=0x7f090012;
-        public static final int btn_clear=0x7f090013;
-        public static final int btn_find=0x7f090006;
-        public static final int btn_get=0x7f090016;
-        public static final int btn_maint=0x7f09001a;
-        public static final int btn_startwo=0x7f090019;
-        public static final int btn_store=0x7f090015;
+        public static final int action_settings=0x7f09001e;
+        public static final int btn_Show=0x7f090016;
+        public static final int btn_cancel=0x7f090014;
+        public static final int btn_clear=0x7f090015;
+        public static final int btn_find=0x7f090008;
+        public static final int btn_get=0x7f090018;
+        public static final int btn_maint=0x7f09001c;
+        public static final int btn_startwo=0x7f09001b;
+        public static final int btn_store=0x7f090017;
         public static final int button1=0x7f090000;
-        public static final int checkBox1=0x7f09000c;
-        public static final int checkBox2=0x7f09000d;
-        public static final int checkBox3=0x7f09000e;
-        public static final int checkBox4=0x7f09000f;
-        public static final int checkBox5=0x7f090010;
+        public static final int checkBox1=0x7f09000e;
+        public static final int checkBox2=0x7f09000f;
+        public static final int checkBox3=0x7f090010;
+        public static final int checkBox4=0x7f090011;
+        public static final int checkBox5=0x7f090012;
         public static final int pager=0x7f090001;
         public static final int plannerTitle=0x7f090002;
+        public static final int planner_list_plannedWorkout=0x7f090006;
+        public static final int planner_list_suggestedExercises=0x7f090005;
         public static final int planner_muscleGroupSpinner=0x7f090004;
-        public static final int question=0x7f090007;
-        public static final int radio0=0x7f090009;
-        public static final int radio1=0x7f09000a;
-        public static final int radio2=0x7f09000b;
-        public static final int radioGroup1=0x7f090008;
-        public static final int section_label=0x7f09001b;
+        public static final int question=0x7f090009;
+        public static final int radio0=0x7f09000b;
+        public static final int radio1=0x7f09000c;
+        public static final int radio2=0x7f09000d;
+        public static final int radioGroup1=0x7f09000a;
+        public static final int section_label=0x7f09001d;
         public static final int textView1=0x7f090003;
-        public static final int textViewURL=0x7f090011;
-        public static final int webLoc=0x7f090017;
-        public static final int webLocation=0x7f090018;
-        public static final int workout_info=0x7f090005;
+        public static final int textViewURL=0x7f090013;
+        public static final int webLoc=0x7f090019;
+        public static final int webLocation=0x7f09001a;
+        public static final int workout_info=0x7f090007;
     }
     public static final class layout {
         public static final int activity_splash_screen=0x7f030000;
@@ -71,6 +73,7 @@ public final class R {
         public static final int song=0x7f040000;
     }
     public static final class string {
+        public static final int ListFont=0x7f060007;
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int hello_world=0x7f060002;
@@ -98,6 +101,8 @@ public final class R {
         Base application theme for API 14+. This theme completely replaces
         AppBaseTheme from BOTH res/values/styles.xml and
         res/values-v11/styles.xml on API 14+ devices.
+
+
     
  API 14 theme customizations can go here. 
          */
--- Final_Project/src/plannerfragment/PlannerFragment.java
+++ Final_Project/src/plannerfragment/PlannerFragment.java
@@ -10,11 +10,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 public class PlannerFragment extends Fragment {
 	private Spinner muscleGrpSpinner_;
+	private ListView suggestedExercisesList_;
+	private ListView plannedWorkoutList_;
 
 	public PlannerFragment() {
 
@@ -27,17 +32,70 @@ public class PlannerFragment extends Fragment {
 				false);
 		muscleGrpSpinner_ = (Spinner) rootView
 				.findViewById(R.id.planner_muscleGroupSpinner);
+		suggestedExercisesList_ = (ListView) rootView
+				.findViewById(R.id.planner_list_suggestedExercises);
+		plannedWorkoutList_ = (ListView) rootView
+				.findViewById(R.id.planner_list_plannedWorkout);
+
+		createSpinnerList();
+		createSuggestedExerciseListView();
+		createPlannedWorkoutListView();
+
+		return rootView;
+	}
+
+	public void createSpinnerList() {
 		List<String> list = new ArrayList<String>();
 		// Add the muscle group here
 		list.add("list1");
 		list.add("list2");
 		list.add("list3");
+
 		ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(
 				getActivity(), android.R.layout.simple_spinner_item, list);
 		spinnerAdapter
 				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		muscleGrpSpinner_.setAdapter(spinnerAdapter);
+	}
 
-		return rootView;
+	// Need to improve this function after finishing the pop-up
+	public void createSuggestedExerciseListView() {
+		List<String> list = new ArrayList<String>();
+		int i = 1;
+		while (i < 11) {
+			list.add("ex" + i);
+			i++;
+		}
+		suggestedExercisesList_.setAdapter(new ArrayAdapter<String>(
+				getActivity(), android.R.layout.simple_list_item_1, list));
+		suggestedExercisesList_
+				.setOnItemClickListener(new OnItemClickListener() {
+
+					@Override
+					public void onItemClick(AdapterView<?> arg0, View arg1,
+							int arg2, long arg3) {
+
+					}
+				});
+	}
+
+	// Need to improve this function after finishing the pop-up
+	public void createPlannedWorkoutListView() {
+		List<String> list = new ArrayList<String>();
+		int i = 1;
+		while (i < 11) {
+			list.add("workout plan" + i);
+			i++;
+		}
+		plannedWorkoutList_.setAdapter(new ArrayAdapter<String>(getActivity(),
+				android.R.layout.simple_list_item_1, list));
+		plannedWorkoutList_.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+
+			}
+		});
 	}
 }
