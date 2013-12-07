29d976ecb56b8d34e8b4321b0534531618d06009
saeryunl <saeryunl@vt.edu>
2013-12-06 20:11:45 -0500

    Fix me retrieve network task

--- Final_Project/src/com/ece4564/group11/workout/network/GetDataNetworkTask.java
+++ Final_Project/src/com/ece4564/group11/workout/network/GetDataNetworkTask.java
@@ -9,11 +9,15 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
 import org.json.simple.parser.ContainerFactory;
 import org.json.simple.parser.JSONParser;
 
@@ -38,7 +42,6 @@ public class GetDataNetworkTask extends AsyncTask<Void, Void, List<String>> {
 	public GetDataNetworkTask(String address, String uuid, PlannerFragment pf) {
 		uuid_ = uuid;
 		address_ = address;
-
 		pf_ = pf;
 	}
 
@@ -54,32 +57,34 @@ public class GetDataNetworkTask extends AsyncTask<Void, Void, List<String>> {
 					response.getEntity().getContent()));
 			Log.d("Get status:", response.getStatusLine().toString());
 			JSONParser parser = new JSONParser();
-			ContainerFactory containerFactory = new ContainerFactory() {
-				public List creatArrayContainer() {
-					return new LinkedList();
-				}
-
-				public Map createObjectContainer() {
-					return new LinkedHashMap();
-				}
-			};
-			Map json = (Map) parser.parse(reader, containerFactory);
-			Iterator iter = json.entrySet().iterator();
-			cache_ = new HashMap<String, String>();
-			System.out.println("==iterate result==");
-
-			Map.Entry<String, String> entry = null;
-			while (iter.hasNext()) {
-				entry = (Map.Entry<String, String>) iter.next();
-				String value = entry.getValue();
-				Log.d("VALUES: ", entry.getKey() + "=>" + value);
-
-				cache_.put(entry.getKey(), value);
-				System.out.println(cache_);
-				result.add(entry.getKey().toString());
-
-				System.out.println("What key was retrieved? " + returnKey_);
+			try {
+				JSONObject jOb = (JSONObject) parser.parse(reader);
+				Log.d("Parsed JSONObject", jOb.toString());
+				Iterator itr = (Iterator) jOb.keySet().iterator();
+			} catch (ParseException e) {
+				e.printStackTrace();
 			}
+			/*
+			 * ContainerFactory containerFactory = new ContainerFactory() {
+			 * public List creatArrayContainer() { return new LinkedList(); }
+			 * 
+			 * public Map createObjectContainer() { return new LinkedHashMap();
+			 * } }; Map json = (Map) parser.parse(reader, containerFactory);
+			 * Iterator iter = json.entrySet().iterator(); cache_ = new
+			 * HashMap<String, String>();
+			 * System.out.println("==iterate result==");
+			 * 
+			 * Map.Entry<String, String> entry = null; while (iter.hasNext()) {
+			 * entry = (Map.Entry<String, String>) iter.next(); String value =
+			 * entry.getValue(); Log.d("VALUES: ", entry.getKey() + "=>" +
+			 * value);
+			 * 
+			 * cache_.put(entry.getKey(), value); System.out.println(cache_);
+			 * result.add(entry.getKey().toString());
+			 * 
+			 * System.out.println("What key was retrieved? " + returnKey_); }
+			 */
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
